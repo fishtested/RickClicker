c0590e31283e13f2dec2db56150eb790679cfb96
fishtested <123904088+fishtested@users.noreply.github.com>
2025-04-13 23:30:56 -0400

    Added click counting

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+let ricks = 0;
+
 function rickClick() {
     var clickaudio = document.getElementById("clickaudio");
         clickaudio.play();
+    ricks = ricks + 1;
+    document.title = `${ricks} Ricks - RickClicker`;
 }
